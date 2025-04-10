f88d9ca34123631a7094bced18303d39f050536f
Vivek205 <ichbinvivek@gmail.com>
2025-04-10 17:31:49 +0200

    handle SQL error

--- app/data/data.ts
+++ app/data/data.ts
@@ -4,17 +4,22 @@ import { User, UserModel } from "./types";
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: "require" });
 
 export const getAllUser = async (): Promise<User[]> => {
-  const users = await sql<
-    UserModel[]
-  >`SELECT id, name, email, password, thumbnail_src, thumbnail_alt, username, bio from USERS`;
-  return users.map(
-    ({ id, name, username, thumbnail_alt, thumbnail_src, bio }) => ({
-      content: bio,
-      id,
-      name,
-      thumbnailAlt: thumbnail_alt,
-      thumbnailSrc: thumbnail_src,
-      username,
-    })
-  );
+  try {
+    const users = await sql<
+      UserModel[]
+    >`SELECT id, name, email, password, thumbnail_src, thumbnail_alt, username, bio from USERS`;
+    return users.map(
+      ({ id, name, username, thumbnail_alt, thumbnail_src, bio }) => ({
+        content: bio,
+        id,
+        name,
+        thumbnailAlt: thumbnail_alt,
+        thumbnailSrc: thumbnail_src,
+        username,
+      })
+    );
+  } catch (error) {
+    console.log("SQL Error", error);
+    return [];
+  }
 };
